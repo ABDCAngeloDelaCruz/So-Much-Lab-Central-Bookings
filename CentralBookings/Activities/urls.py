6629385f6b94a72ace087af3b04168e43d4e333d
Neil Dustin Benedict Agner <neil.agner@student.ateneo.edu>
2024-12-01 00:42:51 +0800

    Added Activity List
    This reverts commit 37cf17dcf6413804e98c2c087fae82c408f51649.

--- CentralBookings/Activities/urls.py
+++ CentralBookings/Activities/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.activities_view, name="home")
+    path("", views.activities_list_view, name="home"),
+    path("organizer/", views.organizer_info_list_view, name="organizer_info"),
+    path("participant-booking/", views.participant_activity_booking_list_view, name="participant_booking"),
 ]
--- CentralBookings/Activities/views.py
+++ CentralBookings/Activities/views.py
@@ -1,15 +1,23 @@
+from django.db import connection
 from django.shortcuts import render
 
 # relative import of forms
-from .models import Activity
+from .models import Activity, Organizer, Participant, Booking, Department
 
 
-def activities_view(request):
-    # dictionary for initial data with 
-    # field names as keys
-    context = {}
-
-    # add the dictionary during initialization
-    context["dataset"] = Activity.objects.all()
+def activities_list_view(request):
+    cursor = connection.cursor()
+    #
+    # query = "SELECT o.Organizer_Name, a.Activity_Name, a.Location, a.Date, a.Start_Time, a.End_Time, a.Expected_Participants FROM organizer o, activity a WHERE o.Organizer_ID = a.Organizer_ID"
+    # filter_date = 
+    #
+    cursor.execute("SELECT o.Organizer_Name, a.Activity_Name, a.Location, a.Date, a.Start_Time, a.End_Time, a.Expected_Participants FROM organizer o, activity a WHERE o.Organizer_ID = a.Organizer_ID;")
+    query = cursor.fetchall()
         
-    return render(request, "activities.html", context)
+    return render(request, "activities.html", {'data': query})
+
+def organizer_info_list_view():
+    return
+
+def participant_activity_booking_list_view():
+    return
