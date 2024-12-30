5577ba8ccc6db5830ab1c124b23dae5402892de6
angelo-dlcrz <angelo.delacruz@student.ateneo.edu>
2024-12-31 00:49:54 +0800

    Added a README

--- CentralBookings/cbpostgres.sql
+++ CentralBookings/cbpostgres.sql
@@ -1,6 +1,6 @@
--- CREATE DATABASE cenbookdb;
+CREATE DATABASE cenbookdb;
 
--- \c cenbookdb
+\c cenbookdb
 
 CREATE TABLE organizer (
 	Organizer_ID SERIAL PRIMARY KEY,
@@ -53,72 +53,72 @@ CREATE TABLE booking (
 );
 
 
--- INSERT INTO organizer (Organizer_Name, Organizer_Type, Organizer_Address, Contact_Person_Given_Name, Contact_Person_Middle_Initial, Contact_Person_Last_Name,
--- Contact_Email, Contact_Number)
--- VALUES ('TechEx Organizers', 'Internal', 'Katipunan, Quezon City', 'Harith', 'D', 'Magiba', 't1@gmail.com', '09092200909'),
---        ('Earth is Love Organization', 'External', 'Barangka, Marikina', 'Feli', 'Z', 'Navidad', 'mrbeast@yahoo.com', '09998883856'),
---        ('Math Workshop Organizers', 'Internal', 'Tondo, Manila', 'Gusion', null, 'Delos Santos', 'teamsecret@gmail.com', '09998881234'),
---        ('Chinese For Everyone', 'External', 'Binondo, Manila', 'Alice', null, 'Calugay', 'aguo@pogo.com', '09355541234'),
---        ('Engineers and Innovators', 'External', 'Poblacion, Makati', 'Bill', null, 'Zuckerburg', 'bz@tech.com', '09678889209'),
---        ('Bio is Life', 'Internal', 'Katipunan, Quezon City', 'Charles', 'H', 'Dawin', 'cdawin@gmail.com', '09448731309'),
---        ('Humanities Lovers', 'Internal', 'Katipunan, Quezon City', 'Michelle', null, 'Reyes-Guo', 'mreyes@gmail.com', '09355364234'),
---        ('Scholars Hub', 'Internal', 'Katipunan, Quezon City', 'Reg', 'I', 'Strar', 'regstrar@gmail.com', '09357824599');
+INSERT INTO organizer (Organizer_Name, Organizer_Type, Organizer_Address, Contact_Person_Given_Name, Contact_Person_Middle_Initial, Contact_Person_Last_Name,
+Contact_Email, Contact_Number)
+VALUES ('TechEx Organizers', 'Internal', 'Katipunan, Quezon City', 'Harith', 'D', 'Magiba', 't1@gmail.com', '09092200909'),
+       ('Earth is Love Organization', 'External', 'Barangka, Marikina', 'Feli', 'Z', 'Navidad', 'mrbeast@yahoo.com', '09998883856'),
+       ('Math Workshop Organizers', 'Internal', 'Tondo, Manila', 'Gusion', null, 'Delos Santos', 'teamsecret@gmail.com', '09998881234'),
+       ('Chinese For Everyone', 'External', 'Binondo, Manila', 'Alice', null, 'Calugay', 'aguo@pogo.com', '09355541234'),
+       ('Engineers and Innovators', 'External', 'Poblacion, Makati', 'Bill', null, 'Zuckerburg', 'bz@tech.com', '09678889209'),
+       ('Bio is Life', 'Internal', 'Katipunan, Quezon City', 'Charles', 'H', 'Dawin', 'cdawin@gmail.com', '09448731309'),
+       ('Humanities Lovers', 'Internal', 'Katipunan, Quezon City', 'Michelle', null, 'Reyes-Guo', 'mreyes@gmail.com', '09355364234'),
+       ('Scholars Hub', 'Internal', 'Katipunan, Quezon City', 'Reg', 'I', 'Strar', 'regstrar@gmail.com', '09357824599');
 
--- INSERT INTO activity (Activity_Name, Date, Location, Start_Time, End_Time, Expected_Participants, Organizer_ID)
--- VALUES ('Tech Conference', '2023-11-01', 'Convention Center', '09:00:00', '17:00:00', 50, 1),
---        ('LaTeX for Dummies', '2023-11-02', 'Room A', '14:00:00', '16:00:00', 30, 3),
---        ('Blockchain 101', '2023-11-02', 'Room C', '14:00:00', '16:00:00', 20, 1),
---        ('Basics of Building a Bridge', '2023-11-02', 'SEC B - 213', '14:00:00', '16:00:00', 15, 5),
---        ('How to Become a Cloud Engineer in 60 Days', '2023-11-02', 'New Rizal Library 5th Floor', '14:00:00', '16:00:00', 40, 1),
---        ('Acting Workshop','2023-11-03', 'Covered Courts', '15:00:00', '17:00:00', 10, 7),
---        ('How to Protect Our Planet','2023-11-05', 'Faura', '13:00:00', '15:00:00', 30, 2),
---        ('Microplastics in our Body','2023-11-05', 'SEC B - 113', '13:00:00', '15:00:00', 20, 6),
---        ('Chinese Language and Culture Workshop', '2024-12-09', 'SOM 305', '10:00:00', '12:30:00', 30, 4),
---        ('Philosophy Symposium', '2023-12-11', 'Humanities Center', '13:30:00', '16:30:00', 45, 7),
---        ('Fundamentals of Computer Vision', '2023-12-7', 'Faura AVR', '8:30:00', '17:30:00', 18, 1);
+INSERT INTO activity (Activity_Name, Date, Location, Start_Time, End_Time, Expected_Participants, Organizer_ID)
+VALUES ('Tech Conference', '2023-11-01', 'Convention Center', '09:00:00', '17:00:00', 50, 1),
+       ('LaTeX for Dummies', '2023-11-02', 'Room A', '14:00:00', '16:00:00', 30, 3),
+       ('Blockchain 101', '2023-11-02', 'Room C', '14:00:00', '16:00:00', 20, 1),
+       ('Basics of Building a Bridge', '2023-11-02', 'SEC B - 213', '14:00:00', '16:00:00', 15, 5),
+       ('How to Become a Cloud Engineer in 60 Days', '2023-11-02', 'New Rizal Library 5th Floor', '14:00:00', '16:00:00', 40, 1),
+       ('Acting Workshop','2023-11-03', 'Covered Courts', '15:00:00', '17:00:00', 10, 7),
+       ('How to Protect Our Planet','2023-11-05', 'Faura', '13:00:00', '15:00:00', 30, 2),
+       ('Microplastics in our Body','2023-11-05', 'SEC B - 113', '13:00:00', '15:00:00', 20, 6),
+       ('Chinese Language and Culture Workshop', '2024-12-09', 'SOM 305', '10:00:00', '12:30:00', 30, 4),
+       ('Philosophy Symposium', '2023-12-11', 'Humanities Center', '13:30:00', '16:30:00', 45, 7),
+       ('Fundamentals of Computer Vision', '2023-12-7', 'Faura AVR', '8:30:00', '17:30:00', 18, 1);
        
  
--- INSERT INTO department (Department_Name)
--- VALUES ('Computer Science'),
---        ('Mathematics'),
---        ('Biology'),
---        ('Engineering'),
---        ('Environmental Science'),
---        ('Chinese Studies'),
--- 	   ('Humanities');
+INSERT INTO department (Department_Name)
+VALUES ('Computer Science'),
+       ('Mathematics'),
+       ('Biology'),
+       ('Engineering'),
+       ('Environmental Science'),
+       ('Chinese Studies'),
+	   ('Humanities');
 
--- INSERT INTO participant (Participant_Given_Name, Participant_Middle_Initial, Participant_Last_Name, Birth_Date, Participant_Type, Department_ID)
--- VALUES ('Juan', 'B', 'Dela Cruz', '1990-05-15', 'Student', 1),
---        ('Faker', 'M', 'Santos', '1985-03-20', 'Faculty', 1),
---        ('Charles', 'P', 'Leclerc', '1995-07-10', 'Staff Member', 2),
---        ('Lewis', null, 'Hamilton', '1985-01-07', 'Faculty', 3),
---        ('Wanwan', 'A', 'Reyes', '2003-08-15', 'Student', 7),
---        ('Tigreal', 'M', 'Guadalupe', '2002-06-15', 'Student', 5),
---        ('Grock', NULL, 'Sy', '1985-09-20', 'Faculty', 4),
---        ('Cathy', 'L', 'Brown', '1995-03-12', 'Staff Member', 6),
---        ('Hylos', 'R', 'Garcia', '1990-11-10', 'Faculty', 7),
---        ('Emily', NULL, 'Davis', '2000-04-05', 'Student', 2);
+INSERT INTO participant (Participant_Given_Name, Participant_Middle_Initial, Participant_Last_Name, Birth_Date, Participant_Type, Department_ID)
+VALUES ('Juan', 'B', 'Dela Cruz', '1990-05-15', 'Student', 1),
+       ('Faker', 'M', 'Santos', '1985-03-20', 'Faculty', 1),
+       ('Charles', 'P', 'Leclerc', '1995-07-10', 'Staff Member', 2),
+       ('Lewis', null, 'Hamilton', '1985-01-07', 'Faculty', 3),
+       ('Wanwan', 'A', 'Reyes', '2003-08-15', 'Student', 7),
+       ('Tigreal', 'M', 'Guadalupe', '2002-06-15', 'Student', 5),
+       ('Grock', NULL, 'Sy', '1985-09-20', 'Faculty', 4),
+       ('Cathy', 'L', 'Brown', '1995-03-12', 'Staff Member', 6),
+       ('Hylos', 'R', 'Garcia', '1990-11-10', 'Faculty', 7),
+       ('Emily', NULL, 'Davis', '2000-04-05', 'Student', 2);
  
--- INSERT INTO booking (Activity_ID, Participant_ID, Has_Attended)
--- VALUES (1, 1, 'YES'),
---        (1, 2, 'YES'),
---        (1, 3, 'YES'),
---        (1, 10, 'NO'),
---        (2, 3, 'YES'),
---        (2, 8, 'YES'),
---        (2, 4, 'NO'),
--- 	   (4, 5, 'YES'),
---        (1, 4, 'NO'),
---        (6, 3, 'YES'),
---        (4, 10, 'YES'),
--- 	   (10, 1, 'NO'),
---        (8, 2, 'YES'),
---        (5, 3, 'YES'),
---        (7, 10, 'NO'),
--- 	   (7, 1, 'YES'),
---        (5, 2, 'YES'),
---        (4, 7, 'NO'),
---        (11, 10, 'YES');
+INSERT INTO booking (Activity_ID, Participant_ID, Has_Attended)
+VALUES (1, 1, 'YES'),
+       (1, 2, 'YES'),
+       (1, 3, 'YES'),
+       (1, 10, 'NO'),
+       (2, 3, 'YES'),
+       (2, 8, 'YES'),
+       (2, 4, 'NO'),
+	   (4, 5, 'YES'),
+       (1, 4, 'NO'),
+       (6, 3, 'YES'),
+       (4, 10, 'YES'),
+	   (10, 1, 'NO'),
+       (8, 2, 'YES'),
+       (5, 3, 'YES'),
+       (7, 10, 'NO'),
+	   (7, 1, 'YES'),
+       (5, 2, 'YES'),
+       (4, 7, 'NO'),
+       (11, 10, 'YES');
 
 -- SELECT o.Organizer_Name, a.Activity_Name, a.Location, a.Date, 
 -- a.Start_Time, a.End_Time, a.Expected_Participants 
--- README.md
+++ README.md
@@ -1,5 +1,54 @@
-So Much Lab - Central Bookings\
-Abarca, James\
-Agner, Neil Dustin Benedict\
-Dela Cruz, Angelo\
-Lorenzo, Cyril Damon
+# Eagle Hill College Central Bookings
+
+This is a group project for the Information Management class where we created an application which allows users from Eagle Hill College to check out the Organizers of Activities, the Activities that users have booked, and the list of Activities that are available.
+
+## How to Run
+
+1. [Clone](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository) this repository into your working machine.
+2. Make sure that you have [Python](https://www.python.org/downloads/) installed in your machine. It is recommended to use Python 3.
+3. Make sure that you have [PostgreSQL](https://www.postgresql.org/download/) installed in your machine.
+4. Setup a virtual environment for your working directory which must contain this project that was cloned from remote.
+5. Make sure you're in the same directory where the `requirements.txt` is. Run the following in your command line:
+
+```bash
+pip install -r requirements.txt
+```
+
+5. Change your working directory to `CentralBookings/`
+6. Run the following command. Enter the password that you set.
+
+```bash
+psql -U postgres
+```
+
+> **Note**: This is under the assumption that when you set up PostgreSQL, you had a user named `postgres`. If you set up another user, you can use that as well.
+
+6. Run the following command to populate the database. Exit `psql` afterwards.
+
+```pgsql
+\i cbpostgresql.sql
+```
+
+7. Run the following commands:
+
+```bash
+py manage.py makemigrations
+py manage.py migrate
+py manage.py runserver
+```
+
+8. Go to `http://127.0.0.1:8000/`
+
+## How to Use
+
+1. In the Home tab, you can see some information on what Central Bookings is all about.
+2. In the Organizer Information tab, you can select an organizer that you want to check. Their information as well as the activities that they have in store will be shown.
+3. In the Activities List tab, you can find a list of activities based on the filters that you set.
+4. In the Activity Booking Summary tab, you can choose any Participant. You will be shown their information as well as their schedule of activities.
+
+
+Enjoy!
+
+## Credits
+
+Much thanks to [James](https://github.com/kintengg), [Dustin](https://github.com/DustinAgner27), [Gelo](https://github.com/angelo-dlcrz), and [Cyril](https://github.com/CyrilCDL) for making this happen!
